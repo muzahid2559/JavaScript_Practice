4ad726678fc1e9442d8eccb0a880516bb89dd548
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 22:45:07 +0600

    Exercise 02

--- js/script.js
+++ js/script.js
@@ -1,24 +1,68 @@
-// Switch(Conditions)
+//Grade(Exercise 02)
 
-document.writeln("Select an option:\n a. Option 1 \n b. Option 2 \n c. Option 3 ");
+var number = prompt("What is your number?");
+number = parseInt(number);
+var grade;
 
-choice = prompt();
-
-var text;
-
-switch (choice) {
-    case "a": //if(choice=="a")
-        text = "Option 1 Selected!";
-        break;
-    case "b":
-        text = "Option 2 Selected!";
-        break;
-    case "c":
-        text = "Option 3 Selected!";
-        break;
-    default: // else
-        text = "No option is selected!";
-        break;
+if ( number <= 100 && number >= 80 ) {
+    grade = "A+";
+} else if ( number < 80 && number >= 70 ) {
+    grade = "A";
+} else if ( number < 70 && number >= 60 ) {
+    grade = "A-";
+} else if ( number < 60 && number >= 50 ) {
+    grade = "B";
+} else if ( number < 50 && number >= 40 ) {
+    grade = "C";
+} else if ( number < 40 && number >= 33 ) {
+    grade = "D";
+} else if ( number < 30 && number >= 0 ) {
+    grade = "F";
+} else {
+    grade = "Invalid Input!";
 }
 
-document.writeln(text);
+document.write("Your Grade: " + grade);
+
+document.write("Select an Option: \n1. Add \n2. Subtract \n3. Mulitply \n4. Divide");
+
+var num1 = prompt("Enter First Number: ");
+var num2 = prompt("Enter Second Number: ");
+var option = prompt("Choose an operation: ");
+var result = null;
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+option = parseInt(option);
+
+var num1Con = isNaN(num1);
+var num2Con = isNaN(num2);
+var optionCon = isNaN(option);
+
+if (num1Con || num2Con || optionCon) {
+    console.log("Invalid Input!");
+} else {
+    switch (option) {
+        case 1:
+            result = num1 + num2;
+            break;
+        case 2:
+            result = num1 - num2;
+            break;
+        case 3:
+            result = num1 * num2;
+            break;
+        case 4:
+            result = num1 / num2;
+            break;
+        default:
+            break;
+    }
+
+    if (result == null) {
+        document.write("No Result!");
+    }
+    else {
+        document.write("Result: " + result);
+    }
+}
