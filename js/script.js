76b9d1778000cce59cf5a77bdb4a3c39b31a22e9
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-07 00:02:02 +0600

    while loop and for loop

--- js/script.js
+++ js/script.js
@@ -1,68 +1,20 @@
-//Grade(Exercise 02)
 
-var number = prompt("What is your number?");
-number = parseInt(number);
-var grade;
+// while loop
+var i = 1; // initialization
 
-if ( number <= 100 && number >= 80 ) {
-    grade = "A+";
-} else if ( number < 80 && number >= 70 ) {
-    grade = "A";
-} else if ( number < 70 && number >= 60 ) {
-    grade = "A-";
-} else if ( number < 60 && number >= 50 ) {
-    grade = "B";
-} else if ( number < 50 && number >= 40 ) {
-    grade = "C";
-} else if ( number < 40 && number >= 33 ) {
-    grade = "D";
-} else if ( number < 30 && number >= 0 ) {
-    grade = "F";
-} else {
-    grade = "Invalid Input!";
+while ( i<=5 ) { //condition
+   document.write(i);
+    i++; //update
 }
 
-document.write("Your Grade: " + grade);
+document.write("End of while loop!")
+document.write("Start of for loop!");
+// for loop
 
-document.write("Select an Option: \n1. Add \n2. Subtract \n3. Mulitply \n4. Divide");
+for (var j = 1; j<=5 ; j++) {
+    document.write(j);
+}
 
-var num1 = prompt("Enter First Number: ");
-var num2 = prompt("Enter Second Number: ");
-var option = prompt("Choose an operation: ");
-var result = null;
-
-num1 = parseInt(num1);
-num2 = parseInt(num2);
-option = parseInt(option);
-
-var num1Con = isNaN(num1);
-var num2Con = isNaN(num2);
-var optionCon = isNaN(option);
-
-if (num1Con || num2Con || optionCon) {
-    console.log("Invalid Input!");
-} else {
-    switch (option) {
-        case 1:
-            result = num1 + num2;
-            break;
-        case 2:
-            result = num1 - num2;
-            break;
-        case 3:
-            result = num1 * num2;
-            break;
-        case 4:
-            result = num1 / num2;
-            break;
-        default:
-            break;
-    }
-
-    if (result == null) {
-        document.write("No Result!");
-    }
-    else {
-        document.write("Result: " + result);
-    }
+for (var k = 5; k >=1; k-- ) {
+    document.write(k);
 }
