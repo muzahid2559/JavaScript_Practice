357c5f3d865285066953c33aed4b8cda4eb32b4e
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 22:01:42 +0600

    Switch(Conditions)

--- js/script.js
+++ js/script.js
@@ -1,46 +1,24 @@
-//Conditions(if else)
+// Switch(Conditions)
 
-  
-//Find Age
-  var age = prompt();
-  
-  if ( age >= 50 ) {
-       document.writeln("Old!");
-  } else if ( age < 50 && age >= 30 ) {
-    document.writeln("Under 50 but above or equal 30!")
-  } // 30 <= age < 50
-  else if (age < 30 && age >= 18 ) {
-    document.writeln("Under 30 but above or equal 18");
-  } else if ( age < 18 && age > 0) {
-    document.writeln("Under 18!");
-  } else {
-    document.writeln("Invalid Input!");
-  }
-  
-  
+document.writeln("Select an option:\n a. Option 1 \n b. Option 2 \n c. Option 3 ");
 
+choice = prompt();
 
-  //Find the largest number
-  var n1 = prompt("First Number: ");
-  var n2 = prompt("Second Number: ");
-  var n3 = prompt("Third Number: ");
-  
-  n1 = parseInt(n1);
-  n2 = parseInt(n2);
-  n3 = parseInt(n3);
-  
-  if (n1 >= n2) {
-      if (n1 >= n3) {
-        document.writeln(n1 + " is the largest!");
-      }
-      else {
-        document.writeln(n3 + " is the largest!");
-      }
-  } else {
-      if (n2 >= n3) {
-        document.writeln(n2 + " is the largest!");
-      }
-      else {
-        document.writeln(n3 + " is the largest!");
-      }
-  }
+var text;
+
+switch (choice) {
+    case "a": //if(choice=="a")
+        text = "Option 1 Selected!";
+        break;
+    case "b":
+        text = "Option 2 Selected!";
+        break;
+    case "c":
+        text = "Option 3 Selected!";
+        break;
+    default: // else
+        text = "No option is selected!";
+        break;
+}
+
+document.writeln(text);
