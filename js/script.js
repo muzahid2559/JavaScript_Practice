9b5a4036337afbda5b0720212928dd35bb6e495f
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-07 00:35:09 +0600

    For-in and For-of

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
-// String and array iteration
+// For-in string/array/object
+// For-of string/array
 let name = "I am learning Javascript!";
 let food = ["Cake", "Chocolate", "Ice Cream"];
-let len = food.length;
 
-for(var i = 0; i < len; i++) {
-    document.writeln(`Index : ${i}`);
-    document.writeln(food[i]); 
+//object doesn't have index
+let person = {
+    name: "Shakib Al Hasan",
+    profession: "Cricketer",
+    team: "Bangladesh",
+    age: 33,
 }
+
+// We will get index
+ for( var x in name ) {
+    document.writeln(`index ${x} and item ${ name[x] }` );
+} 
+
+
+// We will get items/values
+for(var x of name ) {
+    document.writeln(x);
+}
+
+// We will get indexes
+for (var y in food) {
+    document.writeln(`Index: ${y}; and item: ${food[y]}`);
+}
+
+// We will get items
+for (var y of food) {
+    document.writeln(y);
+}
+
+
+// For-in with objects
+for (var x in person) {
+    document.writeln(`Property: ${x}
+    Value: ${person[x] }`);
+}
+
+// For-of won't work for objects
