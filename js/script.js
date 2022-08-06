68c1098b1e8bda2989bfbc825c47180a2e4870ff
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 16:16:05 +0600

    Arithmetic Operator

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-//Variable
-var a;
-var b;
-a = 45;
+//Operator  =  +, -, *, / 
+// Operand Operator Operand 
+// a++; a=a+1;
+
+document.write(3+3);
+document.write(9-8);
+document.write(5*6);
+document.write(8/4);
+document.write(19%2);
+document.write(2**3);
+
+
+// Increment 
+var a = 5;
+var b = 7;
+a++;
+b++;
 document.write(a);
-a = 56;
-b = "Muzahid"
 document.write(b);
-document.write(a);
 
 
-var x = 30;
-var y = 60;
-var c;
-c = x + y;
+var E , F;
+var c = 6;
+var d = 8;
+E = c++;
+F = ++d;
+document.write(E);
 document.write(c);
+document.write(F);
+
+
 
 
 
