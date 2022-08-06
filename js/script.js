39c37c349ed8804cbfec4f39e2687389c8b951ad
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 15:36:56 +0600

    Syntax and Comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-window.alert("I am Learning JavaScript");
-document.write("I am Learning JavaScript");
-document.write("JS File is connected");
+//Sntax
+document.writeln("I am Learning JavaScript");
+document.writeln("JS File 'is' connected");
+document.writeln('JS File "is" connected');
+document.writeln(`JS File "is" connected
+                  Bangladesh is country`);
+document.writeln(710);
+
+//single comments
+//document.writeln("I am Learning JavaScript"); commentS
+
+/*multiple comments
+document.writeln("JS File 'is' connected");
+document.writeln('JS File "is" connected');*/
