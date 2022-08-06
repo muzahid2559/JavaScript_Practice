a5b6b6cc2b04809bf8fe9d52e9b1d571d4c6f099
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 15:57:03 +0600

    Variable Declare

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-//user input
-var myName;
-myName = prompt("Enter Your Name: ");
-document.write(myName);
+//Variable
+var a;
+var b;
+a = 45;
+document.write(a);
+a = 56;
+b = "Muzahid"
+document.write(b);
+document.write(a);
+
+
+var x = 30;
+var y = 60;
+var c;
+c = x + y;
+document.write(c);
+
 
 
