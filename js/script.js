a70cf59c186d4587a2a1c869a5a4b70926104fbe
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 21:34:11 +0600

    Primitive Types and Reference Types

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-// Objects(Data Types)
-student = {name:"Rahim", age:25, hometown:"Dhaka"}
+// Primitive Types  deals with value and work at number, string, boolean
+//difference address
+let a = 7;
+let b = a;
 
-document.writeln(student["name"]);
-document.writeln(student.age);
+a = 45;
+document.writeln(b);
 
 
-
-student["occu"] = "Student"
-document.writeln(student.occu);
-
-delete student.occu
-document.writeln(student);
-
+// Reference Types  deals with address and work at array,object
+//same address
+let numbers = [1,2,3];
+let newNumbers = numbers;
+document.writeln(newNumbers);
+numbers[1] = 500;
+document.writeln(numbers);
+document.writeln(newNumbers);
