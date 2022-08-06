5f8f28e7f706522ef85724c881c7a8f0b05c0e92
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-07 00:07:15 +0600

    break and continue

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
+// break 
 
-// while loop
-var i = 1; // initialization
-
-while ( i<=5 ) { //condition
-   document.write(i);
-    i++; //update
+for(var i=1; i<=5; i++ ) {
+    if ( i == 3) {
+        break;
+    }
+    document.write(i);
 }
 
-document.write("End of while loop!")
-document.write("Start of for loop!");
-// for loop
 
-for (var j = 1; j<=5 ; j++) {
-    document.write(j);
-}
 
-for (var k = 5; k >=1; k-- ) {
-    document.write(k);
+// continue
+
+for (var i =1; i <= 10; i++){
+    if(i % 2 == 1) {
+        continue;
+    }
+    document.write(i);
 }
