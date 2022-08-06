705ddc7ed68af277e28f9cd87ec9906fc367a8ee
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 17:47:17 +0600

    Exercise 01

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
-// Comparison Operator
-document.write(4 == 4);
-document.write(4 == 6);
-document.write(4 == "4"); //value same
-document.write(4 === "4"); //value same but type is not same
-document.write(4 != 6);
-document.write(4 !== "4");
-document.write(4<5);
+// Convert Celcius to Fahrenheit Temperature
 
-//Logical Operator ( ||, && )
-document.write(true || true);
-document.write(false || true);
-document.write(false && true);
-document.write(true && true);
+var c = prompt("please enter a temperature : ");
+var f = 9/5*c+32;
+alert("Fahrenheit : "+ f +"Degrees");
+document.write("Task Complete");
 
-
-//Conditional Operator
-var a = (1<8)? "Hello" : "world";
-document.write(a);
 
