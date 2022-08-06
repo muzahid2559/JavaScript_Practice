212586ce890d0aadf3ee6914c78907e2c16860b3
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 17:35:45 +0600

    Comparison,Logical and Conditional Operator

--- js/script.js
+++ js/script.js
@@ -1,34 +1,20 @@
-// String Operator
-var line1 = "Hello ";
-var line2 = "World! ";
-document.write(line1 + line2);
+// Comparison Operator
+document.write(4 == 4);
+document.write(4 == 6);
+document.write(4 == "4"); //value same
+document.write(4 === "4"); //value same but type is not same
+document.write(4 != 6);
+document.write(4 !== "4");
+document.write(4<5);
 
-var line3 = 40;
-var line4 = "World! ";
-document.write(line3 + line4);
+//Logical Operator ( ||, && )
+document.write(true || true);
+document.write(false || true);
+document.write(false && true);
+document.write(true && true);
 
 
-var line5 = "Hello";
-var line6 = 40;
-var line7 = 5;
-document.writeln(line5 + line6 + line7);
-
-
-var line6 = 40;
-var line7 = 5;
-var line8 = "Hello ";
-document.write(line6 + line7 + line8);
-
-var line9 = "40";
-var line10 = "5";
-document.writeln(line9 - line10);
-
-var line11 = "40";
-var line12= "5";
-document.writeln(line11 + line12); //batikrom
-
-var line9 = "40";
-var line10 = "Muzahid";
-document.writeln(line9 - line10);
-
+//Conditional Operator
+var a = (1<8)? "Hello" : "world";
+document.write(a);
 
