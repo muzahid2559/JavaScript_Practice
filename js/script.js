6bd6e033cd208a2b38121705946cd5e9a1143632
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-06 18:13:25 +0600

    Numbers(Data Types)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-// Convert Celcius to Fahrenheit Temperature
+// Number(Data Type)
+var a = 3.156;
+var b = 123e5;
+document.writeln(a);
+document.writeln(b);
 
-var c = prompt("please enter a temperature : ");
-var f = 9/5*c+32;
-alert("Fahrenheit : "+ f +"Degrees");
-document.write("Task Complete");
+//convert decemal to binary 
+var c = 150;
+document.writeln(c.toString(2)); 
 
+//dosomiker por koto ghor dekhte cai
+var x = 7.5689;
+document.writeln(x.toPrecision(3));
 
+//string to int
+var y = "15.68";
+document.writeln(parseInt(y));
+
+//string to float
+var z = "15.68";
+document.writeln(parseFloat(z));
+
+var h = "gjjhjh";
+document.writeln(isNaN(h));
+
+var j = 5.68;
+document.writeln(isNaN(j));
