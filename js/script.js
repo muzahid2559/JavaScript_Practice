79de159d4988ba95ec852861edf9021075480805
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-07 11:48:07 +0600

    Functions

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-//Exercise 03
-//Sum of the series of square numbers
+// Functions
 
-var n = parseInt(prompt("Enter the number of terms: "));
-var sum = 0;
-var series = "";
+/*1.Built in Functions:
+alert();
+console.log();*/
 
-for (var i = 1; i <= n; i++) {
-    sum += i ** 2;
-    series += (i ** 2).toString();
-    series += " + ";
-    if (i == n) { continue; }
-    
+//2.User Defined Functions
+
+//Declaration of function
+function saySomething() {
+    document.write("Hello!");
+    document.write("I am learning Javascript!");
+    document.write("Muzahid");
 }
 
-document.write(`${series} = ${sum}`);
+//Execution of Function
+saySomething();
