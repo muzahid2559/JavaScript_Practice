8a2dc9ec066e26d514606a97f3eb08ccf8169ced
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-08 23:18:23 +0600

    Introduction to Regular expressions

--- js/script.js
+++ js/script.js
@@ -1,26 +1,43 @@
-console.log("Before Error!");
+// Regular Expression
+// Pattern Matching Technique
+let re;
+let str;
 
-try {
-    test();
-    //undefined.test();
-} catch(err) {
-    //console.log(err);
-    console.log(err.message);
-    console.log(err.name);
-} finally {
-    console.log("I am inside finally!");
-}
+re = /hello/;
+re = /Hello/;
+re = /hello/i; // i = case insensitive
 
-console.log("After Error!");
+console.log(re); //output: /hello/
+console.log(re.source); //output: hello
 
+str = "HelLo World";
+str = "Again Hello World";
+str = "Hell World";
+str = "sdsHellosds World";
+str = "Again Hello World Hello";
+str = "World";
 
-// Error Handling
+// exec() - Returns result in an array or null
+let result = re.exec(str);
 
-let a = 2;
+// test() - true/false
+result = re.test(str);
 
-try {
-    if(a>15) throw "Too Big";
-    else if (a<5) throw "Too small";
-} catch(err) {
-    console.log(err);
-}
+// match() - Returns array or null
+str = "Again Hello World Hello";
+
+result = str.match(re);
+
+// search() - Returns index of the first match or -1
+str = "World";
+
+result = str.search(re);
+
+// replace() - Return new string
+str = "Again Hello World Hello";
+let newstr = str.replace(re, "Hi");
+
+console.log(result);
+console.log(str);
+console.log(re.source);
+console.log(newstr);
