e10fa720f82da048e1a803f4ba4383eb08547278
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-09 01:02:54 +0600

    Character Sets, Quantifier and Grouping(Regular expressions)

--- js/script.js
+++ js/script.js
@@ -8,43 +8,59 @@ re = /lo W/i;
 re = /loW/i;
 
 // Meta Characters
-re = /^hello/; // Must start with   (^ = keret sebble)
+re = /^hello/; // Must start with
 re = /hello$/; // Must end with
 re = /world$/;
 re = /^hello$/; // Must start and end with
-re = /^h.llo$/; // Matches any one character (. ar akhane je kono akta character thakei hbe)
-re = /h.llo/;              
-re = /h*llo/; // 0 or more times (* ar akhane je kono  character thakeo hbe,na thakleo hbe)
-re = /he?a?llo/; // Optional (? ar age ja thakbe sudu seta thakteo pare abar nao pare but oikhane onno kichu thakte pabe na)
-re = /hello\?/; // escaping  (qustion mark tao akta character. so, hello? thaklei macth korbe)  
+re = /^h.llo$/; // Matches any one character
+re = /h.llo/;
+re = /h*llo/; // 0 or more times
+re = /he?a?llo/; // Optional
+re = /hello?/; // escaping
 
-str = "Again hello World";
-str = "hello hello";
-str = "hello";
-str = "hallo";
-str = "hillo";
-str = "h llo";
-str = "hllo";
-str = "hillo worlde";
-str = "hello worlde";
-str = "hillo";
-str = "hilo";
-str = "hhfsdhfsuillo";
-str = "hello";
-str = "hllo";
-str = "hallo";
-str = "htllo";
-str = "heallo";
-str = "hello";
-str = "hallo";
-str = "hllo";
-str = "hello";
-str = "hello?";
+// Character Set using Brackets []
+re = /h[eai]llo/; // Must be one of them inside brackets
+re = /[HA]ello/i;
+re = /[^ha]ello/; // Anything except those inside brackets (h and a sara onno kichu)
+re = /^[ha]ello/; // Must start with h or a
+re = /[A-Z]ello/; // Start with Uper Case
+re = /^[A-Z]/; // First Letter must be upper case
+re = /^[a-z]/;
+re = /[A-Za-z]ello/;
+re = /[0-9]ello/;
+re = /^[0-9]ello/;
+re = /[^0-9]ello/; // Not digit
+re = /^[0-9][0-9][0-9]ello/; // three digits
+re = /^[0-9][0-9][0-9][0-9][0-9]/ // 5 digits
+
+// Braces {} - Quantifier
+re = /el{2}o/; // Must occur exactly 2 times
+re = /el{3}o/; 
+re = /hel{2,5}o/; // Range
+re = /hel{2,}o/; // At least 2 times
+
+// Parentheses () - Grouping
+re = /^([0-9]){5}/; // /^[0-9][0-9][0-9][0-9][0-9]/ // 5 digits
+re = /^([0-9]xy){4}/;
+
+// Bangladeshi Phone Number
+// total 11 digits
+re = /^01[0-9]{9}$/;
+re = /^+8801[0-9]{9}$/;
+
+
+str = "2xy3xy7xy8xy";
+str = "01788888888";
+str = "+8801811888889";
+
+
+
+
+
 
 
 console.log(re.exec(str));
 reTest(re, str);
-
 function reTest(re, str) {
     if(re.test(str)) {
         console.log(`'${str}' matches '${re.source}'`);
