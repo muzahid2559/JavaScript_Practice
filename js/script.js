8e2153b31335e4dfd78585ea5ae2ace744ed0d35
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-09 01:19:09 +0600

    Shorthand Characters and Assertions (Regular Expression)

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ re = /^h.llo$/; // Matches any one character
 re = /h.llo/;
 re = /h*llo/; // 0 or more times
 re = /he?a?llo/; // Optional
-re = /hello?/; // escaping
+re = /hello\?/; // escaping
 
 // Character Set using Brackets []
 re = /h[eai]llo/; // Must be one of them inside brackets
 re = /[HA]ello/i;
-re = /[^ha]ello/; // Anything except those inside brackets (h and a sara onno kichu)
+re = /[^ha]ello/; // Anything except those inside brackets
 re = /^[ha]ello/; // Must start with h or a
 re = /[A-Z]ello/; // Start with Uper Case
 re = /^[A-Z]/; // First Letter must be upper case
@@ -46,14 +46,39 @@ re = /^([0-9]xy){4}/;
 // Bangladeshi Phone Number
 // total 11 digits
 re = /^01[0-9]{9}$/;
-re = /^+8801[0-9]{9}$/;
+re = /^\+8801[0-9]{9}$/;
+
+// Shorthand Character Classes
+re = /\w/; // Word Character = (alpha, numeric or _ )
+re = /\w+/; // One or more
+re = /\W/; // Non Word Character
+re = /\W+/; // one or more
+re = /\d/; // Digit
+re = /\d+/;
+re = /\D/; // Non digit
+re = /\s/; // Match white space
+re = /\S/; // Match non white space
+re = /Hello\b/; // Word Boundary
+re = /\bHello\b/;
+
+// Assertions
+re = /x(?=yz)/; // Matches x only if x is before y (yz ar age x thaktei hbe)
+re = /x(?!yz)/;     // (yz ar age x thakte pabe na)
 
 
-str = "2xy3xy7xy8xy";
-str = "01788888888";
-str = "+8801811888889";
+str = "fsdfxyzfdsf";
 
 
+console.log(re.exec(str));
+reTest(re, str);
+function reTest(re, str) {
+    if(re.test(str)) {
+        console.log(`'${str}' matches '${re.source}'`);
+    }
+    else {
+        console.log(`'${str}' doesn't match '${re.source}'`);
+    }
+}
 
 
 
