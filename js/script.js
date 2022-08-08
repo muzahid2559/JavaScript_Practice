e380d4fb2c17d5fa0631a02492c8abcbc5db782f
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-08-08 07:28:48 +0600

    Add, Replace and Remove Elements

--- js/script.js
+++ js/script.js
@@ -1,52 +1,62 @@
-// Traversing the DOM
-let val;
+// Adding Element to DOM
+// Create Element
+let olItem = document.createElement('li');
+
+// Add Id and Class
+olItem.className = "a new another-class";
+olItem.id = "new-element";
+
+// Add Attribute
+olItem.setAttribute('title', 'A title to new Element');
+
+olItem.appendChild(document.createTextNode('JavaScript'));
+document.querySelector('ol').appendChild(olItem);
+//console.log(olItem);
+
+let ulItem = document.createElement('li');
+let link = document.createElement('a');
+
+link.appendChild(document.createTextNode('Instagram'));
+link.setAttribute('href', 'https://www.instagram.com');
+ulItem.appendChild(link);
+document.querySelector('ul').appendChild(ulItem);
+console.log(ulItem);
+
+
+// Replacing Elements
+let newHeading = document.createElement('h1');
+newHeading.appendChild(document.createTextNode('H1 New Heading'));
+newHeading.className = "sample-class";
+
+let oldHeading = document.querySelector('h3');
+
+let parent = document.querySelector('.container');
+//parent = oldHeading.parentElement;
+parent.replaceChild(newHeading, oldHeading);
+
+
+console.log(newHeading);
+console.log(oldHeading);
+console.log(parent);
+
+
+// Remove element
+
+let lis = document.querySelectorAll('li');
+lis[0].remove();
+
+
 let list = document.querySelector('ul');
-let listItem = document.querySelector('ul li:first-child');
+list.removeChild(lis[7]);
 
-val = list;
-val = listItem;
+//
+list.classList.add("test");
+list.classList.add("test-new");
+list.classList.remove("test-new");
+let val = list.hasAttribute('title');
+val = list.hasAttribute("class");
+list.setAttribute("title", "Yes")
+list.removeAttribute("title");
+console.log(list);
 
-// Get Child Nodes
-val = list;
-val = list.childNodes;
-val = list.childNodes[0];
-val = list.childNodes[1];
-val = list.childNodes[2];
-val = list.childNodes[1].nodeName;
-val = list.childNodes[0].nodeType;
-
-// 1 Element
-// 2 Attribute
-// 3 Text Node
-// 8 Comment
-// 9 Document Itself
-// 10 Doctype
-val = list.childNodes;
-
-val = list.children;
-val = list.children[1];
-list.children[0].textContent = "Hello";
-val = list.children[1].children[0];
-val = list.children[1].children[0].href;
-
-val = list.firstChild;
-val = list.firstElementChild;
-val = list.lastChild;
-val = list.lastElementChild;
-
-val = list.childElementCount;
-
-val = listItem;
-val = listItem.parentElement;
-val = listItem.parentNode;
-val = listItem.parentElement.parentElement;
-
-val = listItem.nextSibling.nextSibling;
-val = listItem.nextElementSibling;
-val = listItem.nextElementSibling.nextElementSibling;
-
-val = document.querySelector('ul li:last-child');
-val = val.previousSibling;
-val = val.previousElementSibling;
-
-console.log(val);
+//console.log(link);
